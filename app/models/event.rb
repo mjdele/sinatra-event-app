de99ebbb493323f143d7bddbc3717ffacebd0506
mjdele <mike.delehant@gmail.com>
2017-03-08 20:08:07 -0500

    Created migration to edit event column type; added belongs_to relationship to events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   has_many :performers
   has_many :event_venues
   has_many :venues, through: :event_venues
+  belongs_to :user
 end
---
+++ db/migrate/20170309005815_change_type_on_events.rb
@@ -0,0 +1,5 @@
+class ChangeTypeOnEvents < ActiveRecord::Migration[5.0]
+  def change
+    change_column :events, :date, :string
+  end
+end
