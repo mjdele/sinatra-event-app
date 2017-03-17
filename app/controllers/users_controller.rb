cc3ab1671c6d9827287f0f3a536a5fd3b9c44ca4
mjdele <mike.delehant@gmail.com>
2017-03-17 19:41:22 -0400

    Added form to create_user.erb; added post '/signup' route to user_controller; edited welcome message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,22 @@ class UsersController < ApplicationController
     erb :'users/create_user'
   end
 
+  post '/signup' do
+    if params["username"] == "" || params["password"] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(params)
+      session[:id] = @user.id
+      redirect to '/events'
+    end
+  end
+
   get '/login' do
     erb :'users/login'
   end
 
   post '/login' do
-    binding.pry
+
   end
 
   get '/logout' do
