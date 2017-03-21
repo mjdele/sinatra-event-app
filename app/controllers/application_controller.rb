6f9721ebf8ee9e6de08021c6891c8b5d1c93dde5
mjdele <mike.delehant@gmail.com>
2017-03-21 10:25:35 -0400

    Updated applicationcontroller with helper methods logged_in? and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,15 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+ helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
+
+  end
+
 end
