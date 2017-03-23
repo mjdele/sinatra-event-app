ae0fc5cfb9547a571056ff27a15b635b6604b06e
mjdele <mike.delehant@gmail.com>
2017-03-22 20:22:32 -0400

    Updated instance variable name in respective :id routes for performer and venue controlers; added performer & venue href links to respective index.erb

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -1,3 +1,16 @@
 class PerformersController < ApplicationController
-  
+
+  get '/performers' do
+    redirect_if_not_logged_in
+    @performers = Performer.all
+
+    erb :'/performers/index'
+  end
+
+  get '/performers/:id' do
+    redirect_if_not_logged_in
+    @performer = Performer.find_by_id(params[:id])
+    erb :'/performers/show'
+  end
+
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,3 +1,16 @@
 class VenuesController < ApplicationController
-  
+
+  get '/venues' do
+    redirect_if_not_logged_in
+    @venues = Venue.all
+
+    erb :'/venues/index'
+  end
+
+  get '/venues/:id' do
+    redirect_if_not_logged_in
+    @venue = Venue.find_by_id(params[:id])
+    erb :'/venues/show'
+  end
+
 end
